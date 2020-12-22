fc04cbbc2636a6561a49c2880dac2d43daed6e10
Antti Pham <antti.pham@gmail.com>
2020-12-23 00:31:23 +0200

    animationutils insertduration has default value. Delay removed from Fade

--- src/components/Animation/Fade.js
+++ src/components/Animation/Fade.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
-import { baseUnit, insertDuration } from './animationUtils'
+import { insertDuration } from './animationUtils'
 
 const Fade = styled.div`
-  transition: opacity ${insertDuration(7*baseUnit)}ms ease-in-out ${insertDuration(3*baseUnit)}ms;
+  transition: opacity ${insertDuration()}ms ease-in-out;
   opacity: ${({ entering, entered }) => entering || entered ? 1 : 0};
 `
 
